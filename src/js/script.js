c6972b3d96071113bc2e51be8389a2046abeb786
kozim-rasulov <kozim_7484443@mail.ru>
2021-06-26 15:52:45 +0500

    v1.3.4 Tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,67 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        /* adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
-      });
-  });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         /* adaptiveHeight: true, */
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                   dots: true,
+//                   arrows: false
+//                 }
+//             }
+//         ]
+//       });
+//   });                            /* Slick-slider */
           
+const slider = tns({                                                              /* Tiny-slider start */
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    speed: 1200,
+
+    responsive: {
+        575: {
+          edgePadding: 20,
+          gutter: 20,
+          items: 1,
+        },
+        767: {
+          gutter: 30
+        },
+        991: {
+          items: 1,
+        },
+        1200: {
+          items: 1
+        }
+      }
+});
+  document.querySelector('.prev').addEventListener ('click', function () {
+    slider.goTo('prev');
+  });
+  document.querySelector('.next').addEventListener ('click', function () {
+    slider.goTo('next');
+  });                                                                       /* Tiny-slider end */
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('.prev').addEventListener ('click', function () {
+    slider.goTo('prev');
+});
