2dcbf538ffee37e96a01adf60bd8ebf0386470c9
kozim-rasulov <86041948+kozim-rasulov@users.noreply.github.com>
2021-07-29 17:38:33 +0500

    scroll animation + page-up

--- src/js/script.js
+++ src/js/script.js
@@ -202,5 +202,20 @@ const slider = tns({                                                            
       return false;
     });
 
+
+    /* Smooth scroll and page-up */
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1200) {
+          $('.page-up').fadeIn();
+      } else {
+          $('.page-up').fadeOut();
+      }
+    });
+
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+
   });
-  
